809aa392e9cad535f7b0ac21dfe78ecd525d5f90
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-27 19:53:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   end
 
    def self.first_x_students_in_grade_10(x)
-    sql = "SELECT * FROM students WHERE grade = 10"
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT = ?"
     DB[:conn].execute(sql, x)
   end
 
